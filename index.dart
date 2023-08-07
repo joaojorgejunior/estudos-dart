6cb2bc11dff4609dd9def7305ebff05e3c2a7698
João Jorge <jn60246@gmail.com>
2023-08-07 20:01:18 -0300

    first commit

---
+++ index.dart
@@ -0,0 +1,61 @@
+// //variaveis do tipo Strings ou seja textos//
+// void main(){
+//   String nome = "João";
+//   nome ='Lucas';
+//   String sobrenome= 'junior';
+
+//   print(nome + sobrenome);
+
+//   String cor = 'Azul';
+//   String resultado = cor ;
+//   print(resultado);
+
+// }
+
+
+// //variaveis do tipo NUMBER ou seja números //
+// void main(){
+//   int valor = 10; //valores inteiros Usar o int //
+//   print(valor);
+
+//   double valorQuebrado = 5.5; //valores quebrados Usar o double //
+//   print(valorQuebrado);
+// } 
+
+
+//variaveis do tipo BOOL ou seja boleanos true ou false//
+// void main ()
+// {
+  
+//   bool isTrue = 1 < 0;
+//   // print(isTrue);
+
+//   bool status = true; //bool//
+//   String resultado;    //String//
+//   resultado = status ? "Verdadeiro": "Falso";
+//   print(resultado);
+//   // O Resultado será diacordo com que o Booleano for definido//
+// }
+
+//variáveis do tipo  *Var* (Não pode ré atribuir a mesma variável) ou *Dynamic* (Já no Dynamic pode atribuir number como string) //
+
+// void main(){
+//   var nome = "João";
+//   var numero = 50;
+//   var peso = 10.5;
+//   var status = true; 
+   
+//   // nome = "String";
+
+//   dynamic sobreNome = 'Junior';
+//   dynamic valor = 10.5;
+
+//   sobreNome = 200;
+//   print(sobreNome);
+
+// }
+
+// Operadores Matemáticos //
+
+
+
