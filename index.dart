bc1ef3238dc131b3438132c7b560f08268f1ee7a
João Jorge <jn60246@gmail.com>
2023-08-08 18:41:21 -0300

    Estudo de DART

--- index.dart
+++ index.dart
@@ -95,3 +95,102 @@
 
 //   print(cor);
 // }
+
+// Criando MAP no Dart //
+// void main(){
+//   Map usuario = {"nome":"joão","idade": "37", "dev":"true"}; 
+//   Map <int,dynamic> nomes = {1:"João", 2:"Jorge", 3:"Junior",4: 50, 5:true}; //MAP com atributos (int e Dynamic) pois adiciona qualquer valor a string.//
+//   print(usuario);
+//   print(nomes);
+
+//   print(usuario['dev']); //confirmando o valor Dev
+
+//   usuario["idade"]= 30; // Alterando a idade do usuário//
+//   print(usuario);
+//   print(usuario.length); //*Length* Para saber o tamanho do map//
+
+// }
+
+//Criadores de concdições no DART //
+  //COMPARADORES//
+  // > MAIOR
+  // >= MAIOR IGUAL
+  // < MENOR
+  // <= MENOR IGUAL
+  // == IGUAL
+  // != DIFERENTE
+
+// void main() {
+
+//  var valor = true; // Atribuindo o valor para variável TRUE//
+
+//  if(valor == true){         //Primeira condição: O valor é igual a True? Resp. Sim!
+//   print("Valor é $valor");  // Emprime esse valor //
+//  }else if(valor == false){  //Segunda condição:  O valor é igual a True? //
+//   print("Valor é $valor");
+//  }else{
+//   print("O valor não é verdadeiro e nem falso"); // Caso não atenda nenhuma das opções anteriores, imprima esse valor//
+//  } 
+// }
+
+
+// Decisão Com SWITCH //
+// void main(){
+//   String statusOficina = "aberto"; // Atribuindo o valor para variável TRUE//
+
+//   switch(statusOficina){           // O maior diferencial do SWITCH para o If estar em relação ao case pois ele determina aonde houverá a parada.
+//     case 'aberto':
+//     print("Seu carro esta status de Aberto");
+//     break;
+//     case 'andamento':
+//     print("Seu carro esta status de Andamento");
+//     break;
+//     case 'pendente':
+//     print("Seu carro esta status de Pendente");
+//     break;
+//     default:
+//       print("Seu carro estar status de finalizado!!!");
+//   }
+// }
+
+  // Laços de repetição com FOR usado para loop e fazer calculos //
+
+//  void main (){
+//  for(int i = 0; i <= 5; i++){
+//   print(i);
+//  }
+//  } 
+
+// void main (){
+//  List numero = [1,2,3,4,5]; //lista de Array//
+//  for (int i = 4; i <= numero.length; i++){ //Ele vai rodar até o tamanho ou último intem da lista número 5 do array//
+//   // print(numero);
+//  } 
+//     for(var i in numero){ //desta forma aqui ele separa um de cada vez //
+//       print(i);
+//     }
+
+// }
+
+// Laços de repetição com whiler //
+
+  // void main(){
+  //   int valor = 5;
+  //   while(valor <= 10){
+  //     print("O valor é $valor");
+  //     valor++; //Limitador para não cair no loop infinito//
+  //   }
+  // }
+
+// Laços de repetição com DO WHILER //
+
+void main(){
+ int valor = 5;
+
+ do{
+    print("o valor é $valor");
+    valor++;           //Limitador para não cair no loop infinito//
+ } while(valor <= 10); 
+
+
+}
